cb6b178e81326e6c6545ac3d84cebbadf2907ac3
fullgreen <abin030187@naver.com>
2016-12-29 05:24:41 +0900

    Revert "healthd: Fix libhealthd.exynos4 build"
    This reverts commit c73f9557c24c3eed3203ada410e03ce4bc62c24c.

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -18,7 +18,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_exynos4.cpp
 LOCAL_MODULE := libhealthd.exynos4
-LOCAL_C_INCLUDES := \
-	system/core/healthd \
-	system/core/healthd/include/healthd
+LOCAL_C_INCLUDES := system/core/healthd
 include $(BUILD_STATIC_LIBRARY)
