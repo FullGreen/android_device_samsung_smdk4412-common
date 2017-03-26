de4c6875fe653988f782289feceb6e80f071869b
fullgreen <abin030187@naver.com>
2017-03-26 12:51:39 +0900

    smdk4412-common: update flags for hardware
    Signed-off-by: fullgreen <abin030187@naver.com>

--- common.mk
+++ common.mk
@@ -66,6 +66,9 @@ PRODUCT_PACKAGES := \
     Gello \
     gralloc.exynos4 \
     hwcomposer.exynos4 \
+    libExynosHWCService \
+    libExynosIPService \
+    libhdmiclient \
     libfimc \
     libfimg \
     libhwconverter \
@@ -171,9 +174,7 @@ PRODUCT_COPY_FILES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=1 \
-    ro.opengles.version=131072 \
-    ro.bq.gpu_to_cpu_unsupported=1 \
-    debug.hwui.render_dirty_regions=false
+    ro.opengles.version=131072
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
